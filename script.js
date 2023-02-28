82c1184bb051ee1613a9e48830ccd780721d9414
ashaqragit <109609168+ashaqragit@users.noreply.github.com>
2023-02-28 15:39:45 +0200

    building the form and linking it to js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const titleInput = document.querySelector("#title");
+const authorName = document.querySelector("#author");
+const isbnCode = document.querySelector("#isbn");
+const submitButton = document.querySelector("#submit-book-btn");
 
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("default prevented");
+});
