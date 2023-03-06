5af501aaa2fb5e346025903769536140b76ed5f8
ashaqragit <109609168+ashaqragit@users.noreply.github.com>
2023-03-06 13:39:47 +0200

    code for adding books

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const isbnCode = document.querySelector("#isbn");
 const submitButton = document.querySelector("#submit-book-btn");
 const didReadYes = document.querySelector("#yes-read");
 const didReadNo = document.querySelector("#no-read");
-const bookCardsContainer = document.querySelector("#bookCards");
+const bookCardsContainer = document.querySelector("#book-list");
 
 let didRead = false;
 
@@ -27,13 +27,34 @@ function checkIfRead() {
   }
 }
 
+//function to create a book object and append to book list array
+function addBookToArray() {
+  let newBook = new book(titleInput.value, authorName.value, didRead);
+  //add the new book object to book listarray
+  bookList.push(newBook);
+  // console.log(bookList);
+}
+//function loop thru booklist and generate HTML and add it to the DOM
+function addBooksToDOM() {
+  for (book of bookList) {
+    bookCardsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div>
+        <h3>book name : the hobbit</h3>
+        <h3>book author: Tolkin</h3>
+        <h3>did you read the book? Yes</h3>
+        <button>delete</button>
+        <button>edit</button>
+        <hr>
+      </div>`
+    );
+  }
+}
+
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   //call the function to check if use read the book
   checkIfRead();
-  //create new book object
-  let newBook = new book(titleInput.value, authorName.value, didRead);
-  //add the new book object to booklist array
-  bookList.push(newBook);
-  console.log(bookList);
+  //create new book object and add to array bookList
+  addBookToArray();
 });
