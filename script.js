0b5324df560a8b32038c6a383d25f3cb0dec7a4a
ashaqragit <109609168+ashaqragit@users.noreply.github.com>
2023-03-06 14:50:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,47 @@ const didReadYes = document.querySelector("#yes-read");
 const didReadNo = document.querySelector("#no-read");
 const bookCardsContainer = document.querySelector("#book-list");
 
-let didRead = false;
-
+let didRead = "";
 let bookList = [];
+let bookIdList = [];
+let bookId = "";
 
 class book {
-  constructor(name, author, didRead) {
+  constructor(bookId, name, author, didRead) {
+    this.bookId = bookId;
     this.name = name;
     this.author = author;
-    this.read = didRead;
+    this.didRead = didRead;
   }
 }
 
 // function deceleration to check if use read the book
 function checkIfRead() {
   if (didReadYes.checked) {
-    didRead = true;
+    didRead = "Yes";
   } else if (didReadNo.checked) {
-    didRead = false;
+    didRead = "No";
+  }
+}
+
+//function generate uniqe book id and check if the id exist if yes generate a new id
+function bookIdGenerate() {
+  let newBookId = `${Math.floor(Math.random() * 100)}-${Math.floor(
+    Math.random() * 100
+  )}`;
+  console.log(newBookId);
+  bookIdList.push(newBookId);
+  console.log(bookIdList);
+  for (book of bookList) {
+    if (bookIdList.includes(newBookId) === false) {
+      bookId = newBookId;
+      console.log("new book id");
+      console.log(bookIdList);
+    } else if (bookIdList.includes(newBookId) === true) {
+      console.log("id already exist generating new one");
+      bookIdList.pop();
+      bookIdGenerate();
+    }
   }
 }
 
@@ -40,9 +63,9 @@ function addBooksToDOM() {
     bookCardsContainer.insertAdjacentHTML(
       "beforeend",
       `<div>
-        <h3>book name : the hobbit</h3>
-        <h3>book author: Tolkin</h3>
-        <h3>did you read the book? Yes</h3>
+        <h3>book name : ${book.name}</h3>
+        <h3>book author: ${book.author}</h3>
+        <h3>did you read the book? ${book.didRead}</h3>
         <button>delete</button>
         <button>edit</button>
         <hr>
@@ -50,11 +73,13 @@ function addBooksToDOM() {
     );
   }
 }
-
+//function to delete HTML element and remove the corresponding object in booklist array
+function deleteBook(bookId) {}
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   //call the function to check if use read the book
   checkIfRead();
   //create new book object and add to array bookList
   addBookToArray();
+  bookIdGenerate();
 });
